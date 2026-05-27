6a94718e6c8ec710969aa727664694bc69cdcedb
s4mmy_work <sammyLinsj@outlook.com>
2026-05-27 17:51:55 +0800

    react_update

--- my_react_agent.py
+++ my_react_agent.py
@@ -46,4 +46,58 @@ class MyReactAgent(ReActAgent):
         max_steps: int = 5,
         custom_prompt: Optional[str] = None
 
-    )
+    ):
+        super().__init__(name, llm,system_prompt, config)
+        self.tool_registry = tool_registry
+        self.max_steps = max_steps
+        self.current_history: List[str] = []
+        self.prompt_template = custom_prompt if custom_prompt else MY_REACT_PROMPT
+        print(f"✅ {name} 初始化完成，最大步数: {max_steps}")
+
+    def run(self, input_text: str, **kwargs) -> str:
+        """运行ReAct Agent"""
+        self.current_history = []
+        current_step = 0
+
+        print(f"\n🤖{self.name}开始处理问题{input_text}")
+
+        while current_step < self.max_steps:
+            current_step +=1
+            print(f"\n--- 第 {current_step} 步 ---")
+
+            # 1. 构建提示词
+            tools_desc = self.tool_registry.get_tools_description()
+            history_str = "\n".join(self.current_history)
+            prompt = self.prompt_template(
+                tools = tools_desc,
+                question = input_text,
+                histtory = history_str
+            )
+
+            #调用LLM
+            messagess = [{"role":"user","content":prompt}]
+            response_text = self.llm.invoke(messagess, **kwargs)
+
+            #解析输出
+            thought, action = self._parse_output(response_text)
+
+            #检查完成条件
+
+            if action and action.startswith("Finish"):
+                final_answer = self._parse_action_input(action)
+                self.add_message(Message(input_text,"user"))
+                self.add_message(Message(final_answer,"assistant"))
+                return final_answer
+            
+            #执行工具使用
+            if action:
+                tool_name, tool_input = self._parse_action(action)
+                observation = self.tool_registry.execute_tool(tool_name, tool_input)
+                self.current_history.append(f"Action: {action}")
+                self.current_history.append(f"Observation: {observation}")
+        
+        # 达到最大步数
+        final_answer = "抱歉，我无法在指定部署内完成这个任务"
+        self.add_message(Message(input_text,"user"))
+        self.add_message(Message(final_answer,"assistant"))
+        return final_answer
